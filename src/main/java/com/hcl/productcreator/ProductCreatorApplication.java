436fa945a42297579f8be6a611fda2a112396016
Alejandro Linan <jandrolinan@gmail.com>
2021-01-26 10:02:37 -0600

    restored some imports

--- src/main/java/com/hcl/productcreator/ProductCreatorApplication.java
+++ src/main/java/com/hcl/productcreator/ProductCreatorApplication.java
@@ -1,9 +1,12 @@
 package com.hcl.productcreator;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ProductCreatorApplication {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		SpringApplication.run(ProductCreatorApplication.class, args);
 	}
 
